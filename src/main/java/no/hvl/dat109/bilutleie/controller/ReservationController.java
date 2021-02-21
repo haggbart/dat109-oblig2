2a21a4d3efdb6f2c046f3c46c993ae7e926d39a7
Roger Karlsen <mr.haggbart@gmail.com>
2021-02-21 04:26:38 +0100

    working offerselect. Closing #17

--- src/main/java/no/hvl/dat109/bilutleie/controller/ReservationController.java
+++ src/main/java/no/hvl/dat109/bilutleie/controller/ReservationController.java
@@ -3,17 +3,16 @@ package no.hvl.dat109.bilutleie.controller;
 import lombok.extern.slf4j.Slf4j;
 import no.hvl.dat109.bilutleie.dto.ReservationDto;
 import no.hvl.dat109.bilutleie.dto.ReservationForLocationTimeDto;
-import no.hvl.dat109.bilutleie.model.RentalOffice;
+import no.hvl.dat109.bilutleie.model.CarCategory;
 import no.hvl.dat109.bilutleie.service.CarService;
 import no.hvl.dat109.bilutleie.service.RentalOfficeService;
+import no.hvl.dat109.bilutleie.service.ReservationService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -26,55 +25,62 @@ public class ReservationController {
 
     private final CarService carService;
     private final RentalOfficeService officeService;
+    private final ReservationService reservationService;
+    private final ModelMapper mapper;
 
-    public ReservationController(CarService carService, RentalOfficeService officeService) {
+    public ReservationController(CarService carService, RentalOfficeService officeService, ReservationService reservationService, ModelMapper mapper) {
         this.carService = carService;
         this.officeService = officeService;
-    }
-
-
-    @GetMapping("/offerselect")
-    public String locationTimeForm(HttpSession session, Model model) {
-
-        var reservation = (ReservationDto) session.getAttribute("reservation");
-
-        model.addAttribute("city", reservation.getPickup().getAddress().getCity());
-        model.addAttribute("cars", carService.getCarsByOffice(reservation.getPickup()));
-
-        return "offerselect";
+        this.reservationService = reservationService;
+        this.mapper = mapper;
     }
 
     @PostMapping("/locationtime")
     public String locationTimeSubmit(@Valid @ModelAttribute("locationTime") ReservationForLocationTimeDto locationTime,
-                                     BindingResult bindingResult,
-                                     Model model,
-                                     HttpSession session) {
+                                     BindingResult bindingResult, Model model, HttpSession session) {
 
         if (bindingResult.hasErrors() || !validDates(locationTime, bindingResult)) {
-            System.out.println("FAAAAAAAAAAIL");
             model.addAttribute("offices", officeService.getOffices());
             return "index";
         } else {
             System.out.println("NO ERRORS");
         }
 
-        RentalOffice pickup = officeService.getOffice(locationTime.getPickup());
         ReservationDto reservation = new ReservationDto();
-        reservation.setPickup(pickup);
-        System.out.println(pickup);
-        session.setAttribute("reservation", reservation);
-        log.debug("startDate: {}", locationTime.getStartDate());
-        log.debug("endDate: {}", locationTime.getEndDate());
+        mapper.map(locationTime, reservation);
 
+        session.setAttribute("reservation", reservation);
         session.setAttribute("locationTime", locationTime);
         return "redirect:/reservation/offerselect";
     }
 
+    @GetMapping("/offerselect")
+    public String offerSelectForm(HttpSession session, Model model) {
+
+        var reservation = (ReservationDto) session.getAttribute("reservation");
+        model.addAttribute("categories", carService.availableCategories(reservation.getPickup(),
+                reservation.getStartDate(), reservation.getEndDate()));
+        model.addAttribute("city", reservation.getPickup().getAddress().getCity());
+        model.addAttribute("cars", carService.getCarsByOffice(reservation.getPickup()));
+        return "offerselect";
+    }
+
+    @PostMapping("/offerselect")
+    public String selectOffer(@RequestParam String category, HttpSession session) {
+        var reservation = (ReservationDto) session.getAttribute("reservation");
+        reservation.setCarCategory(CarCategory.valueOf(category));
+        return "redirect:/reservation/details";
+    }
+
+    @GetMapping("/details")
+    public String detailsForm(Model model, HttpSession session) {
+        model.addAttribute("reservation", (ReservationDto) session.getAttribute("reservation"));
+        return "details";
+    }
+    
     private boolean validDates(ReservationForLocationTimeDto locationTime, BindingResult bindingResult) {
         if (locationTime.getStartDate().isBefore(locationTime.getEndDate())) return true;
-        bindingResult.addError(new FieldError(
-                "locationTime",
-                "endDate",
+        bindingResult.addError(new FieldError("locationTime", "endDate",
                 "Leveringstid må være etter utlevering"));
         return false;
     }
--- src/main/java/no/hvl/dat109/bilutleie/dto/ReservationForLocationTimeDto.java
+++ src/main/java/no/hvl/dat109/bilutleie/dto/ReservationForLocationTimeDto.java
@@ -1,6 +1,7 @@
 package no.hvl.dat109.bilutleie.dto;
 
 import lombok.Data;
+import no.hvl.dat109.bilutleie.model.RentalOffice;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
@@ -13,9 +14,9 @@ public class ReservationForLocationTimeDto {
     private final static String DATETIME_PATTERN = "dd/MM/yyyy HH:mm";
 
     @NotNull
-    private Long pickup;
+    private RentalOffice pickup;
 
-    private Long dropoff;
+    private RentalOffice dropoff;
 
     @NotNull(message = "Velg tid for utlevering")
     @DateTimeFormat(pattern = DATETIME_PATTERN)
